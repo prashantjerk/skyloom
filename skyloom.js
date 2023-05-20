24dc0ed8af280fd8566f7dcc484c7397188977d0
Prashant <prashantkarki630@gmail.com>
2023-05-20 10:23:34 +0545

    transition added, opacity for phones distinguished

--- skyloom.js
+++ skyloom.js
@@ -1,20 +1,3 @@
-// var swiperSlider = new Swiper(".swiper-slider", {
-//     effect: "coverflow",
-//     grabCursor: true,
-//     centeredSlides: true,
-//     loop: true,
-//     slidesPerView: "auto",
-//     coverflowEffect: {
-//       rotate: 0,
-//       stretch: 0,
-//       depth: 100,
-//       modifier: 2,
-//       slideShadows: true,
-//     },
-//     slideActiveClass: 'swiper-slide-active',
-//     slideNextClass: 'swiper-slide-next'
-//   });
-
 var swiperSlider = new Swiper(".swiper-slider", {
   effect: "coverflow",
   grabCursor: true,
