e41914691887019b76cdeb272bdf698869cf8fec
Prashant <prashantkarki630@gmail.com>
2023-05-20 16:03:43 +0545

    final test

--- skyloom.js
+++ skyloom.js
@@ -1,17 +1,21 @@
 var swiperSlider = new Swiper(".swiper-slider", {
-    effect: "coverflow",
-    grabCursor: true,
-    centeredSlides: true,
-    loop: true,
-    slidesPerView: "auto",
-    coverflowEffect: {
-      rotate: 0,
-      stretch: 0,
-      depth: 100,
-      modifier: 2,
-      slideShadows: true,
-    },
-    slideActiveClass: 'swiper-slide-active',
-    slideNextClass: 'swiper-slide-next'
-  });
-  
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  loop: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 0,
+    depth: 100,
+    modifier: 2,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
